6f46d65170ec1d9ed543695030ff58fe226b7dff
Chaminda Indunil <chaminda.indunil@hotmail.com>
2021-09-19 16:41:33 +0530

    insert pg config

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [
-    
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Tanasha@1987',
+      database: 'CMS',
+      synchronize: true
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
